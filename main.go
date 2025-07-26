4527eb9ea8c895cc2935d88afbd0d9e353309a6f
Mwai Banda <bandamwai@gmail.com>
2025-07-26 18:21:25 -0500

    Added godotenv

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+	
+
 	port := func() string {
 		if len(os.Getenv("PORT")) > 0 {
 			return os.Getenv("PORT")
