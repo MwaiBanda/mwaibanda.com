3f8bd14216e3d2078c04d420b052237e883ec2d6
Mwai Banda <bandamwai@gmail.com>
2025-07-26 18:13:53 -0500

    Added support .env

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"net/http"
-	
+	"os"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	port := func() string {
+		if len(os.Getenv("PORT")) > 0 {
+			return os.Getenv("PORT")
+		} else {
+			return "8080"
+		}
+	}()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
